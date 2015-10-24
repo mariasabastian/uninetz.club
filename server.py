b13a708db51b295224fd14933db16ebe85c98897
Maria Priyanka Sabastian <sabastian.maria@gmail.com>
2015-10-24 10:21:02 -0700

    use simple hello world app, introduced support for local development

---
+++ server.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template, jsonify
+import logging
+import json
+import sys
+
+app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+
+# get service information from Bluemix
+if 'VCAP_SERVICES' in os.environ:
+  services = os.environ['VCAP_SERVICES']
+else:
+  with open('services.json') as services_file:    
+    services = json.load(services_file)
+
+@app.route('/')
+def Index():
+  return render_template('index.html')
+
+@app.route('/services')
+def Services():
+  return jsonify(services)
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=int(port))
